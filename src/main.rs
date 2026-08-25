ae4a9b40b57dac252a42851deab080462ae36ac2
RadekParek <r.sobcak.r@gmail.com>
2026-08-25 11:22:38 +0200

    Absolute minimum Rust build

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use android_activity_cpp::{AndroidApp, AndroidActivity};
-
 #[no_mangle]
-fn android_main(app: &mut AndroidApp) {
-    loop {
-        // Native Rust loop
-    }
+pub extern " C\ fn android_main() {
+ loop {}
 }
